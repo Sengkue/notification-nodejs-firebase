dfca16dbac211d28b29b12d57bad2cf507b274f5
sengvang.blo <skvgithub@gmail.com>
2024-09-30 16:24:18 +0700

    commit 30/09/2024 16:24:18.11

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./config/db');
@@ -9,14 +8,12 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-// Sync the database
 sequelize.sync().then(() => {
   console.log("Database synced!");
 }).catch(err => {
   console.error("Unable to sync the database:", err);
 });
 
-// API endpoint to receive and store the token
 app.post('/api/tokens', async (req, res) => {
   const { token } = req.body;
 
@@ -35,7 +32,6 @@ app.post('/api/tokens', async (req, res) => {
   }
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
