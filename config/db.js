2eaa9c40cfd8e9288cf18b999aa6abf0596e14a7
sengvang.blo <skvgithub@gmail.com>
2024-09-30 16:03:07 +0700

    commit 30/09/2024 16:03:07.12

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 
 // Connect to MySQL database
 const sequelize = new Sequelize('notification', 'root', '', {
-  host: 'nod',
+  host: 'localhost',
   dialect: 'mysql',
 });
 
